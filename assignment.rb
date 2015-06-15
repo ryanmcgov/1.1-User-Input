9cf4e9dd9224d3c45aeb64b4cfbc5c09aeb2552f
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-06-15 17:12:26 -0400

    Task Complete
    The assignment is complete.  The app asks for name and response, then
    prints the appropriate response back to the user.

--- assignment.rb
+++ assignment.rb
@@ -2,8 +2,8 @@ puts "What is your name?"
 name = gets.chomp
 #chomp turns "name\n" into just "Name"
 
-p	%{
-	What is your current legal situation(choose one number)?
+puts %{	
+What is your current legal situation(choose one number)?
 		1. I am law abiding!
 		2. I make sure others obey the law.
 		3. I live outside the law.
@@ -12,4 +12,15 @@ p	%{
 
 answer = gets.chomp
 
-puts answer
+if answer == "1" 
+		puts "The Empire thanks you for your aquiescence, citizen #{name}."
+	elsif answer == "2"
+		puts "#{name}, aren't you a little short for a Storm Trooper?"
+	elsif answer == "3"
+		puts "Hey, #{name}! Who's scruffy-lookin'?"
+	elsif answer == "4"
+		puts "#{name}, I loathe bounty hunters. We don't need their scum..."
+	else 
+		puts "#{name}, meesa Jar Jar Binks."
+	end
+
